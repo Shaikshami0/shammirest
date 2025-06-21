0d21f2524b1e5ffaec750f2d913ab24292894af2
Shaikshami0 <22a81a0653@sves.org.in>
2025-06-21 14:47:16 +0530

    shammi

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/menu')
 def menu():
